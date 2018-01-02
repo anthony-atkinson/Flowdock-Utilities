c7bfceeccda00999dd77c75a839ff12bf40e8283
Anthony Atkinson <anthony@anthonyatkinson.info>
2018-01-01 23:26:26 -0700

    1. Removed all the php files. The few requests that still needed it are now being handled by a nodejs app in server/server.js. 2. Changed all the requests in the angular app to point at the nodejs application instead. 3. Added in a way to pull in the removed users using api.flowdock.com/private. If the authenticated user ever had a 1-to-1 private conversation with them, /private should pull in the user object.

--- app/util/FlowdockAuthService.js
+++ app/util/FlowdockAuthService.js
@@ -6,23 +6,21 @@
 angular.module('myApp.FlowdockAuthService', ['myApp.DateUtil']).
 service('FlowdockAuthService', ['$http', '$cookies', 'DateUtil', function($http, $cookies, DateUtil) {
   var serviceRoot = this;
-  var client_id = '';
-  var client_secret = '';
-  var client_redirect_uri_encoded = '';
-  var redirect_url = '';
 
   this.ListOfFlows = [];
   this.ListOfUsers = [];
-  this.userToken = ($cookies.get('userToken') !== null && $cookies.get('userToken') !== undefined) ?
-    $cookies.get('userToken') : undefined;
-  this.access_token = ($cookies.get('access_token') !== null && $cookies.get('access_token') !== undefined) ?
-    $cookies.get('access_token') : undefined;
-  this.refresh_token = ($cookies.get('refresh_token') !== null && $cookies.get('refresh_token') !== undefined) ?
-    $cookies.get('refresh_token') : undefined;
-  this.created_at = ($cookies.get('created_at') !== null && $cookies.get('created_at') !== undefined) ?
-    $cookies.get('created_at') : undefined;
-  this.expires_in = ($cookies.get('expires_in') !== null && $cookies.get('expires_in') !== undefined) ?
-    $cookies.get('expires_in') : undefined;
+  this.ListOfUsersNotAvailable = [];
+  var token_obj = $cookies.get('token_obj');
+  if(token_obj.startsWith('j:')) {
+    token_obj = token_obj.substr(2);
+  }
+  token_obj = JSON.parse(token_obj);
+
+  this.userToken = token_obj.access_token;
+  this.access_token = token_obj.access_token;
+  this.refresh_token = token_obj.refresh_token;
+  this.created_at = token_obj.created_at;
+  this.expires_in = token_obj.expires_in;
 
   function clearOutQuery() {
     if(window.location.search !== undefined && window.location.search.length > 0) {
@@ -31,12 +29,6 @@ service('FlowdockAuthService', ['$http', '$cookies', 'DateUtil', function($http,
         window.location.href = window.location.href.split("?")[0];
       }, 1500);
     }
-  }
-
-  function authenticateWithFlowdock() {
-    // Redirect to get token from Flowdock
-    window.location = 'https://www.flowdock.com/oauth/authorize?client_id=' + client_id +
-      '&redirect_uri=' + client_redirect_uri_encoded + '&response_type=code';
   }
 
   // Taken from http://stackoverflow.com/a/901144 since the angular $location.search()
@@ -61,8 +53,7 @@ service('FlowdockAuthService', ['$http', '$cookies', 'DateUtil', function($http,
       method: 'GET',
       // dataType: 'json',
       headers: { 'Content-Type': 'application/json' },
-      url: location.protocol + '//' + location.hostname + location.port +
-      '/notifier/getFlowNames.php?access_token=' + this.access_token
+      url: 'https://api.flowdock.com/flows?access_token=' + this.access_token
     }).then(function success(response) {
       serviceRoot.ListOfFlows = [];
       var flows = response.data;
@@ -79,21 +70,24 @@ service('FlowdockAuthService', ['$http', '$cookies', 'DateUtil', function($http,
         }
       }else {
         console.log('flows was not an array!');
-        throw 'Data coming back from flows request was not an array like expected!';
+        location.href = '/notifier/api/auth';
       }
 
     }, function error(response){
       console.error('An error has occurred retrieving flow names. See below.');
       console.error(response);
+      console.log("User isn't authorized. Redirecting to /notifier/api/auth to re-auth");
+      location.href = '/notifier/api/auth';
     });
   };
 
   this.refreshUserList = function() {
+    var accToken = this.access_token;
     // Get Flowdock users list
     $http({
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
-      url: 'https://api.flowdock.com/users?access_token=' + this.access_token
+      url: 'https://api.flowdock.com/users?access_token=' + accToken
     }).then(function success(response) {
       serviceRoot.ListOfUsers = [];
       var users = response.data;
@@ -105,136 +99,59 @@ service('FlowdockAuthService', ['$http', '$cookies', 'DateUtil', function($http,
             email: users[i].email,
             avatar: users[i].avatar,
             name: users[i].name,
-            website: users[i].website
+            website: users[i].website,
+            removed: false
           });
         }
+        // Get users from /private (since this can include removed users)
+        $http({
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+          url: 'https://api.flowdock.com/private?access_token=' + accToken
+        }).then(function secondSuccess(response2) {
+          var privateUsers = response2.data;
+          if(Array.isArray(privateUsers)) {
+            for(var i = 0, len = privateUsers.length; i < len; i++) {
+              var arrayUser2 = privateUsers[i];
+              var foundUser = serviceRoot.ListOfUsers.find(function(u) {
+                return arrayUser2.id !== undefined && u.id !== undefined && u.id === arrayUser2.id;
+              });
+              if(foundUser === null || foundUser === undefined) {
+                var foundMainUserObj = arrayUser2.users.find(function(u) {
+                  return arrayUser2.id !== undefined && u.id !== undefined && u.id === arrayUser2.id;
+                });
+                // Use the original arrayUser2 if foundMainUserObj wasn't found.
+                if(foundMainUserObj === null || foundMainUserObj === undefined) {
+                  foundMainUserObj = arrayUser2;
+                }
+                serviceRoot.ListOfUsers.push({
+                  id: foundMainUserObj.id,
+                  nick: foundMainUserObj.nick,
+                  email: foundMainUserObj.email,
+                  avatar: foundMainUserObj.avatar,
+                  name: foundMainUserObj.name,
+                  website: foundMainUserObj.website,
+                  removed: true
+                });
+              }
+            }
+          }
+        }, function secondError(response2) {
+          console.error('An error has occurred retrieving user list from /private. See below.');
+          console.error(response);
+        })
       }else {
         console.log('User list was not an array!');
-        throw 'Data coming back from users request was not an array like expected!';
+        location.href = '/notifier/api/auth';
       }
-
     }, function error(response){
-      console.error('An error has occurred retrieving user list. See below.');
+      console.error('An error has occurred retrieving user list from /users. See below.');
       console.error(response);
+      console.log("User isn't authorized. Redirecting to /notifier/api/auth to re-auth");
+      location.href = '/notifier/api/auth';
     });
   };
 
-  this.getTokensAndFinishSettingUpService = function() {
-    var code = getParameterByName('code');
-    var access_token = getParameterByName('access_token');
-    if(code != null) {
-      serviceRoot.userToken = code;
-      $cookies.put('userToken', serviceRoot.userToken, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-      clearOutQuery();
-      return;
-    }else if(access_token != null) {
-      serviceRoot.access_token = access_token;
-      $cookies.put('access_token', serviceRoot.access_token, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-      $location.search('code', null);
-      clearOutQuery();
-      return;
-    }else if(serviceRoot.userToken !== undefined) {
-      // Do nothing since it is already stored in our cookie
-    }else {
-      authenticateWithFlowdock();
-      return;
-    }
-    // Ask for Flowdock for access_token if local access_token is undefined. Only do so if userToken isn't empty as well.
-    if(serviceRoot.userToken !== undefined && serviceRoot.userToken !== null && serviceRoot.access_token === undefined) {
-      console.log('getting access_token');
-      $http({
-        method: 'POST',
-        crossOrigin: true,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        url: location.protocol + '//' + location.hostname + location.port + '/notifier/getAccessToken.php',
-        data: {
-          client_id: client_id,
-          client_secret: client_secret,
-          code: serviceRoot.userToken,
-          redirect_uri: redirect_url,
-          grant_type: 'authorization_code',
-          provider: $http
-        }
-      }).then(function successCallback(response) {
-        serviceRoot.access_token = response.data.access_token;
-        $cookies.put('access_token', response.data.access_token, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-        serviceRoot.refresh_token = response.data.refresh_token;
-        $cookies.put('refresh_token', response.data.refresh_token, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-        serviceRoot.created_at = new Date(response.data.created_at);
-        $cookies.put('created_at', response.data.created_at, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-        serviceRoot.expires_in = new Date(response.data.created_at + response.data.expires_in);
-        $cookies.put('expires_in', response.data.expires_in, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-        serviceRoot.refreshFlowNames();
-        serviceRoot.refreshUserList();
-      }, function errorCallback(response) {
-        console.error('An error occurred! See below:');
-        console.error(response);
-      });
-    }
-    // Token may need refreshed; Check to see if our current access token has expired
-    else if(serviceRoot.created_at !== undefined && serviceRoot.created_at !== null && serviceRoot.expires_in !== undefined && serviceRoot.expires_in !== null) {
-      console.log('Refreshing Token');
-      var curDate = new Date();
-      if(curDate > serviceRoot.expires_in) {
-        $http({
-          method: 'POST',
-          crossOrigin: true,
-          headers: { 'Content-Type': 'application/json' },
-          url: location.protocol + '//' + location.hostname + location.port + '/notifier/getAccessToken.php',
-          data: {
-            refresh_token: serviceRoot.refresh_token,
-            client_id: client_id,
-            client_secret: client_secret,
-            grant_type: 'refresh_token',
-            provider: $http,
-            ReturnUrl: client_redirect_uri_encoded
-          }
-        }).then(function successCallback(response) {
-          serviceRoot.access_token = response.data.access_token;
-          $cookies.put('access_token', response.data.access_token, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-          serviceRoot.refresh_token = response.data.refresh_token;
-          $cookies.put('refresh_token', response.data.refresh_token, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-          serviceRoot.created_at = new Date(response.data.created_at);
-          $cookies.put('created_at', response.data.created_at, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-          serviceRoot.expires_in = new Date(response.data.created_at + response.data.expires_in);
-          $cookies.put('expires_in', response.data.expires_in, { path: '/', expires: DateUtil.oneMonthFromToday() } );
-
-          serviceRoot.refreshFlowNames();
-          serviceRoot.refreshUserList();
-        }, function errorCallback(response) {
-          console.error('An error occurred! See below:');
-          console.error(response);
-        });
-      }else {
-        console.log('auto-refreshing flow names and users.');
-        serviceRoot.refreshFlowNames();
-        serviceRoot.refreshUserList();
-      }
-    }
-  };
-
-  // Get settings from app_settings.json
-  ($http.get(location.protocol + '//' + location.hostname + location.port + '/app_settings.json')
-      .then(function successCallback(response) {
-          client_id = response.data.client_id;
-          client_secret = response.data.client_secret;
-          redirect_url = response.data.redirect_url;
-          client_redirect_uri_encoded = encodeURIComponent(redirect_url);
-          serviceRoot.getTokensAndFinishSettingUpService();
-        }, function errorCallback(response) {
-          console.info('An error occurred! See below:');
-          console.info(response);
-          throw "Unable to get app_settings.json from server!";
-        }
-      )
-  );
+  serviceRoot.refreshFlowNames();
+  serviceRoot.refreshUserList();
 }]);
